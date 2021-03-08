cf808cf5124b4d7b3e7deb820458ac5e8ba0f206
Tomasz Rutkowski <trutkowski@solwit.com>
2021-03-08 13:16:06 +0100

    Update npm commands and pipeline settings

---
+++ banqsoft-web/karma-cli.conf.js
@@ -0,0 +1,50 @@
+module.exports = function (config) {
+  config.set({
+      basePath: '',
+      frameworks: ['jasmine', '@angular-devkit/build-angular'],
+      plugins: [
+          require('karma-jasmine'),
+          require('karma-chrome-launcher'),
+          require('karma-jasmine-html-reporter'),
+          require('karma-coverage-istanbul-reporter'),
+          require('@angular-devkit/build-angular/plugins/karma')
+      ],
+      client: {
+          clearContext: false // leave Jasmine Spec Runner output visible in browser
+      },
+      jasmineHtmlReporter: {
+        suppressAll: true // removes the duplicated traces
+      },
+      coverageReporter: {
+        dir: require('path').join(__dirname, './coverage/banqsoft-web'),
+        subdir: '.',
+        reporters: [
+          { type: 'html' },
+          { type: 'text-summary' }
+        ]
+      },
+      reporters: ['progress', 'kjhtml'],
+      port: 9876,
+      colors: true,
+      logLevel: config.LOG_INFO,
+      autoWatch: false,
+      browsers: ['ChromeHeadlessNoSandbox'],
+      browserDisconnectTimeout: 10000,
+      browserDisconnectTolerance: 3,
+      browserNoActivityTimeout: 60000,
+      customLaunchers: {
+          ChromeHeadlessNoSandbox: {
+              base: 'ChromeHeadless',
+              flags: [
+                  '--no-sandbox',
+                  '--headless',
+                  '--proxy-server="direct://"',
+                  '--proxy-bypass-list=*',
+                  '--disable-dev-shm-usage'
+              ]
+          }
+      },
+      singleRun: true,
+      restartOnFileChange: false
+  });
+};
