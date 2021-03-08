c9e601c377490ebc8a0ffc4e9f53b0db1e65b16a
Tomasz Rutkowski <trutkowski@solwit.com>
2021-03-08 13:40:56 +0100

    Configure JUnit

--- banqsoft-web/karma-cli.conf.js
+++ banqsoft-web/karma-cli.conf.js
@@ -10,7 +10,8 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-junit-reporter')
     ],
     client: {
       jasmine: {
@@ -33,7 +34,10 @@ module.exports = function (config) {
         { type: 'cobertura' }
       ]
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'junit'],
+    junitReporter: {
+      outputDir: 'junit'
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
